0cfaf945d808dcdb5fc49b5be0953aa78d110daf
Morton Fox <github@qslw.com>
2017-01-06 16:41:59 -0500

    Initial commit

---
+++ source/app.d
@@ -0,0 +1,14 @@
+import vibe.d;
+
+shared static this()
+{
+    auto router = new URLRouter;
+    router.get("/", staticTemplate!"dashboard.dt");
+
+    auto settings = new HTTPServerSettings;
+    settings.port = 8080;
+    settings.bindAddresses = ["::1", "127.0.0.1"];
+    listenHTTP(settings, router);
+
+    logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+}
