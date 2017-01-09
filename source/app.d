d5f225a3d60c301494d8a4f8571d259141f4046f
Morton Fox <github@qslw.com>
2017-01-09 18:12:49 -0500

    Add upload hander. Add session store. Persist timezone selection in session. Set max request size to a generous value.

--- source/app.d
+++ source/app.d
@@ -5,11 +5,14 @@ shared static this()
 {
     auto router = new URLRouter;
     router.get("/", &dashboard);
+    router.post("/upload", &upload);
     router.get("*", serveStaticFiles("public/"));
 
     auto settings = new HTTPServerSettings;
+    settings.sessionStore = new MemorySessionStore;
     settings.port = 8080;
     settings.bindAddresses = ["::1", "127.0.0.1"];
+    settings.maxRequestSize = 50_000_000;
     listenHTTP(settings, router);
 
     logInfo("Please open http://127.0.0.1:8080/ in your browser.");
@@ -17,6 +20,16 @@ shared static this()
 
 void dashboard(HTTPServerRequest req, HTTPServerResponse res) {
     auto tznames = PosixTimeZone.getInstalledTZNames();
-    auto selected_tz = "US/Eastern";
+
+    if (!req.session) req.session = res.startSession();
+    auto selected_tz = req.session.get("tz", "US/Eastern");
+
     render!("dashboard.dt", tznames, selected_tz)(res);
+}
+
+void upload(HTTPServerRequest req, HTTPServerResponse res) {
+    if (!req.session) req.session = res.startSession();
+    req.session.set("tz", req.form.get("timezone"));
+
+    res.redirect("/");
 }
